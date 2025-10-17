3ddfcb36e996beb597e6662f5cbce9bf28d2893b
Ryuuu69 <144459226+Ryuuu69@users.noreply.github.com>
2025-10-17 17:11:31 +0200

    Updated app-build-manifest.json

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("d2eda8033ff415e5")
+/******/ 		__webpack_require__.h = () => ("a3c1ce8d3a643be8")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6155e7313fe5ec3c"; }
+/******/ 		__webpack_require__.h = function() { return "05e447a4adf458ab"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
